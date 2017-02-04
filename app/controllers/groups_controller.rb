4544acd07266ed89026b0ab1d7b59bf84bcdea47
“将叫我将军” <d2194499406@gmail.com>
2017-02-04 13:50:40 +0800

    implement groups#edit_and_update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,10 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
@@ -21,6 +25,16 @@ class GroupsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render :edit
+    end 
   end
 
   private
